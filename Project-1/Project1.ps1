c19a9c8b3a086658df2dc9a323f6449dfe5a8b86
harrison-b24 <143291045+harrison-b24@users.noreply.github.com>
2023-10-09 15:56:49 -0400

    Uploaded project1

---
+++ Project-1/Project1.ps1
@@ -0,0 +1,9 @@
+﻿# Load synthesizer and create synthesizer object
+[System.Reflection.Assembly]::LoadWithPartialName("System.Speech")
+$synthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+# Change this text to be whatever you want the synth to say
+$textToSpeak = "REPLACE THIS TEXT"
+
+# Speak the text
+$synthesizer.Speak($textToSpeak)
